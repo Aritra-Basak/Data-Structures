8acfc0436ad4c1556145c1e9620292e211477ee9
Aritra Basak <basakaritra10@gmail.com>
2022-08-28 21:59:25 +0530

    Update Linked_Hash_Map.java

--- Linked_Hash_Map.java
+++ Linked_Hash_Map.java
@@ -18,6 +18,7 @@ public class L_Hash_Map {
 	        lhm.put("one", "New York");
 	        lhm.put("two", "Iceland");
 	        lhm.put("three", "Tokyo");
+	        lhm.put("four", "Italy");
 	 
 	        // Printing all entries inside Map
 	        System.out.println(lhm);
@@ -58,5 +59,46 @@ public class L_Hash_Map {
 	        // Printing mappings to the console
 	        System.out.println("Mappings of LinkedHashMap : "
 	                           + lhm);
+	        
+	        
+	     // Insertion Order iterating
+	        System.out.println(
+	            "Insertion Order of LinkedHashMap:"
+	            + " iterating \n");
+	  
+	        // getting keySet() into Set, as in Linked Hash Map .keySet(); function is not supported by Iterator
+	        Set<String> set = lhm.keySet();
+	  
+	        // get Iterator from key set
+	        Iterator<String> itr = set.iterator();
+	  
+	        // iterating as per Insertion Order
+	        while (itr.hasNext()) {
+	            String key = itr.next();
+	            System.out.println("Key : " + key + "\t\t"
+	                               + "Value : " + lhm.get(key));
+	        }
+	        
+	        // Reverse of Insertion Order iterating
+	        System.out.println("\n\nReverse of Insertion Order:"
+	                           + " iterating \n");
+	  
+	        // convert to ArrayList of key set
+	        //we are just storing the key set in an ArrayList and reversing that & then with respect to the keys and it's orientation we will print the values.
+	        ArrayList<String> alKeys
+	            = new ArrayList<String>(lhm.keySet());
+	  
+	        // reversing the order of keys
+	        Collections.reverse(alKeys);
+	  
+	        // iterate LHM using reverse order of keys
+	        for (String strKey : alKeys) //using for loop to print the array list so we can print the keys with their respective values
+	        {
+	            System.out.println("Key : " + strKey + "\t\t"
+	                               + "Value : "
+	                               + lhm.get(strKey));
+	        }
+	        
+	        
 	    }
 	}
