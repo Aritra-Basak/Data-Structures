b8f947e1927c3d085eeb8667f15a4827c5ad2817
Aritra Basak <basakaritra10@gmail.com>
2022-09-02 16:35:19 +0530

    Update Stack_Collection.java

--- Stack_Collection.java
+++ Stack_Collection.java
@@ -15,13 +15,25 @@ public class COLLECTION_Stack {
 		    	System.out.println("Is The stack empty?");
 		    	System.out.println(stack.isEmpty());
 		    	System.out.println("Size is :"+ stack.size());
-
-		    	System.out.println("After ");
-		    	for(int i = 1;i<=n;i++)
+		    	
+		    	//iterating in stack
+		        System.out.println("The elements are");
+		    	for( int items :stack)
+		    	{
+		    		System.out.println(items);
+		    	}
+		    	
+		    	System.out.println("Popping out the following:-");
+		    	for(int i = 1;i<=3;i++)
 		    	{
 		    	System.out.println(stack.pop());
 		    	}
 		    	
+		    	System.out.println("After popping:");
+		    	for( int items :stack)
+		    	{
+		    		System.out.println(items);
+		    	}
 		    }
 
 }
