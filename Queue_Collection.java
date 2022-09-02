60051d923acd76f52bb10ae14ee10c74759993b2
Aritra Basak <basakaritra10@gmail.com>
2022-09-02 20:20:30 +0530

    Update Queue_Collection.java

--- Queue_Collection.java
+++ Queue_Collection.java
@@ -13,8 +13,10 @@ public class COLLECTION_QUEUE {
     	{
     		queue.add(i);
     	}
+    	
         System.out.println("Size of the queue is :"+queue.size());
         System.out.println("Head Element is:"+queue.peek());//prints the head of the queue
+        System.out.println( "Head element is:"+ queue.element());//also prints the head of the queue
         System.out.println("Elements are");
         
         //iterating in queue
@@ -27,7 +29,42 @@ public class COLLECTION_QUEUE {
         for (int item: queue) {
             System.out.println(item);
         }
-    
+        //deque:
+        //Unlike normal queue(insertion from rear and removal from front) deque(pronounced as 'deck') a kind of queue where we can insert and remove from both the sides.
+        Deque<Integer> deque = new ArrayDeque <>(); //its faster than normal queue.
+        //ArrayDeque is faster than LinkedList on average. But for adding an element, ArrayDeque takes amortized constant time, and LinkedList takes constant time.
+        //The amortized constant time complexity comes from amortized analysis. This kind of analysis is used when we want to evaluate the total complexity of a sequence of operations
+          
+        System.out.println("DEQUE");
+        for(int i =5;i>=1;i--)
+          {
+        	  deque.addFirst(i);
+          }
+          for(int i =6;i<=10;i++)
+          {
+        	  deque.addLast(i);
+          }
+        
+          for(int items:deque)
+          {
+        	  System.out.println(items);
+          }
+          System.out.println("Size is :"+deque.size());
+          deque.add(100);//when normally added it will be added at the last or rear end of the deque.
+          for(int items:deque)
+          {
+        	  System.out.println(items);
+          }
+          
+         System.out.println("REmoving..:"+deque.removeFirst()); 
+         System.out.println("REmoving..:"+deque.removeLast()); 
+         System.out.println("REmoving..:"+deque.remove()); //when normally removed the element gets removed from front end.
+         System.out.println("Size is :"+deque.size());
+         for(int items:deque)
+         {
+       	  System.out.println(items);
+         }
+        
 	}
 
 }
